a397cd6020bf0d5fad3d11c3794f7a36fcefa98a
Ealden Esto E. Escañan <ealden@dot.ph>
2015-03-20 18:14:34 +0800

    Disable Resque for starting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ set :branch, 'production'
 role :app, %w{deploy@registry.production.local}
 role :db,  %w{deploy@registry-db.production.local}
 
-role :resque_worker, %w{deploy@registry.production.local}
-role :resque_scheduler, %w{deploy@registry.production.local}
+# role :resque_worker, %w{deploy@registry.production.local}
+# role :resque_scheduler, %w{deploy@registry.production.local}
