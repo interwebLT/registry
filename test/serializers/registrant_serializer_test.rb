00bc22bf8308246a57603ed27bdcfdfae4781935
Ealden Esto E. Escañan <ealden@dot.ph>
2015-03-24 14:32:53 +0800

    Use ContactsSerializer instead of RegistrantSerializer as they are largely the same

--- test/serializers/registrant_serializer_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-describe RegistrantSerializer do
-  subject { RegistrantSerializer.new(registrant).serializable_hash }
-
-  let(:registrant) { build :registrant }
-
-  specify { subject[:name].must_equal registrant.name }
-end
