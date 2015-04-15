4cc9aedfdb39ee8bcdde21eb497db7f99b9986e7
Ealden Esto E. Escañan <ealden@dot.ph>
2015-04-15 18:51:09 +0800

    Link OrderDetail::RenewDomain to Domain when completed

--- test/models/order_detail/renew_domain_test.rb
+++ test/models/order_detail/renew_domain_test.rb
@@ -15,6 +15,8 @@ describe OrderDetail::RenewDomain do
 
     specify { subject.complete?.must_equal true }
     specify { saved_domain.expires_at.must_equal '2016-01-01 00:00'.in_time_zone }
+    specify { subject.product.must_equal saved_domain.product }
+
     specify { latest_ledger_entry.activity_type.must_equal 'use' }
     specify { latest_ledger_entry.credits.must_equal -35.money }
   end
