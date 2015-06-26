69a71c84f7497e9d45dfc26decff083a7cc80488
Ealden Esto E. Escañan <ealden@dot.ph>
2015-06-26 12:58:22 +0800

    Ability to restore deleted domains

--- test/models/deleted_domain_test.rb
+++ test/models/deleted_domain_test.rb
@@ -11,5 +11,18 @@ describe DeletedDomain do
     specify { subject.billing_contact.wont_be_nil }
     specify { subject.tech_contact.wont_be_nil }
   end
+
+  describe :restore! do
+    subject { create :deleted_domain, name: domain_name }
+
+    let(:domain_name) { 'test.ph' }
+
+    before do
+      subject.restore!
+    end
+
+    specify { DeletedDomain.exists?(name: domain_name).wont_equal true }
+    specify { Domain.exists?(name: domain_name).must_equal true }
+  end
 end
 
