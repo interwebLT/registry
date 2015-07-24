e45655a0f105f54039f9bc1c9b351a63ec223cde
Ealden Esto E. Escañan <ealden@dot.ph>
2015-07-24 14:41:59 +0800

    Limit to 100 for now

--- app/models/object_activity.rb
+++ app/models/object_activity.rb
@@ -7,6 +7,6 @@ class ObjectActivity < ActiveRecord::Base
   validates :activity_at, presence: true
 
   def self.latest
-    self.all.includes(partner: :credits, product: [:domain, :deleted_domain]).order(id: :desc).limit(500)
+    self.all.includes(partner: :credits, product: [:domain, :deleted_domain]).order(id: :desc).limit(100)
   end
 end
