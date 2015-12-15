f3281c8bfeb21d12ffb7c447c30b543b8c438cf9
Miguel Arguelles <cmarguel@dot.ph>
2015-12-15 16:22:04 +0800

    use params.permit to construct User instead of manually assigning attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
   def create 
+    user = User.new user_params
+    
+    user.registered_at = Time.now
+    user.partner = Partner.find params[:partner_id]
 
-    u = User.new
-    u.email = params[:email]             
-    u.name = params[:name]              
-    u.password = params[:password]
-    u.username = params[:username]          
-    u.registered_at = Time.now
-    u.partner = Partner.find params[:partner_id]
-
-    if u.save
-        render json: u
-    elsif u.errors.added? :email, "already_exists"
-        render validation_failed u
+    if user.save
+      render json: user
+    elsif User.exists?(email: user.email)
+      render validation_failed user
     else
-        render bad_request
+      render bad_request
     end
+  end
+
+  private
+
+  def user_params
+    params.permit :email, :name, :password, :username
   end
 end
