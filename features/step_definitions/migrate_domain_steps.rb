a6f34527fa025620f0fb73da7b5418d5418f4443
Ealden Esto E. Escañan <ealden@dot.ph>
2015-12-19 11:13:26 +0800

    Moved migrate_domain_service into migrate_domain_steps

--- features/step_definitions/migrate_domain_steps.rb
+++ features/step_definitions/migrate_domain_steps.rb
@@ -1,16 +1,24 @@
 When /^I migrate a domain into registry$/ do
-  contact_exists
+  create :contact
 
-  migrate_domain
+  post migrations_path, 'order/admin_migrate_domain_request'.json
 end
 
 When /^I migrate a domain with (.*)$/ do |scenario|
-  migrate_domain scenario: scenario
+  scenarios = {
+    'no domain name'        => 'order/admin_migrate_domain_with_no_domain_name_request',
+    'no registrant handle'  => 'order/admin_migrate_domain_with_no_registrant_handle_request',
+    'no registered at'      => 'order/admin_migrate_domain_with_no_registered_at_request',
+    'no expires at'         => 'order/admin_migrate_domain_with_no_expires_at_request',
+    'expires at before registered at' => 'order/admin_migrate_domain_with_expires_at_before_registered_at_request'
+  }
+
+  post migrations_path, scenarios[scenario].json
 end
 
 Then /^domain must be migrated under non-admin partner$/ do
-  assert_completed_migrate_domain_response
+  last_response.status.must_equal 201
 
-  assert_domain_must_be_registered
-  assert_register_domain_activity_must_be_created
+  Domain.exists? name: DOMAIN
+  Domain.last.domain_activities.first.is_a? ObjectActivity::Create
 end
