93326fdb0a65a3cc85d0a0fbfc0691c849071f07
Ealden Esto E. Escañan <ealden@dot.ph>
2015-12-21 11:57:23 +0900

    Build migrate domain request from scenario

--- features/migrate_domain.feature
+++ features/migrate_domain.feature
@@ -4,14 +4,14 @@ Feature: Migrate Domain
     Given I am authenticated as administrator
 
   Scenario: Migrate domain
-    When  I migrate a domain into registry
+    When  I migrate a domain into registry for another partner
     Then  domain must be migrated under non-admin partner
     And   domain must not have domain hosts by default
     And   domain status must be inactive
     And   migrate domain fee must be deducted from credits of non-admin partner
 
   Scenario Outline: Invalid parameters
-    When  I migrate a domain with <invalid parameter>
+    When  I migrate a domain into registry for another partner with <invalid parameter>
     Then  error must be validation failed
     And   validation error on <field> must be "<code>"
 
--- features/step_definitions/migrate_domain_steps.rb
+++ features/step_definitions/migrate_domain_steps.rb
@@ -1,19 +1,11 @@
-When /^I migrate a domain into registry$/ do
-  create :contact
-
-  post migrations_path, 'order/admin_migrate_domain_request'.json
+MIGRATE_DOMAIN = Transform /^migrate a domain into registry(?: |)(.*?)$/ do |scenario|
+  build_request scenario: scenario, resource: :order, action: :migrate_domain
 end
 
-When /^I migrate a domain with (.*)$/ do |scenario|
-  scenarios = {
-    'no domain name'        => 'order/admin_migrate_domain_with_no_domain_name_request',
-    'no registrant handle'  => 'order/admin_migrate_domain_with_no_registrant_handle_request',
-    'no registered at'      => 'order/admin_migrate_domain_with_no_registered_at_request',
-    'no expires at'         => 'order/admin_migrate_domain_with_no_expires_at_request',
-    'expires at before registered at' => 'order/admin_migrate_domain_with_expires_at_before_registered_at_request'
-  }
+When /^I (#{MIGRATE_DOMAIN})$/ do |request|
+  create :contact
 
-  post migrations_path, scenarios[scenario].json
+  post migrations_path, request.json
 end
 
 Then /^domain must be migrated under non-admin partner$/ do
