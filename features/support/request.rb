318ae38e430e0f1e361bbf0ab5c7580e6a370f10
Ealden Esto E. Escañan <ealden@dot.ph>
2015-12-21 11:50:03 +0900

    Simplified build_request

--- features/support/request.rb
+++ features/support/request.rb
@@ -9,14 +9,8 @@ DOUBLE_SPACE  = '  '
 
 def build_request scenario:, resource:, action:
   request = "#{resource.to_s}/"
-
-  if scenario.include? ADMIN_MARKER
-    request << ADMIN_PREFIX
-    scenario.sub! ADMIN_MARKER, BLANK
-  end
-
-  request << "#{action.to_s} #{scenario.downcase} request"
+  request << ADMIN_PREFIX if scenario.include? ADMIN_MARKER
+  request << "#{action.to_s} #{scenario.sub(ADMIN_MARKER, BLANK).downcase} request"
 
   request.gsub(DOUBLE_SPACE, UNDERSCORE).gsub(DASH, UNDERSCORE).gsub(SPACE, UNDERSCORE)
 end
-
