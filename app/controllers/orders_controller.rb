591d5aec48f26795697f5b05e088a2a30d0dd583
Ealden Esto E. Escañan <ealden@dot.ph>
2016-01-05 13:46:02 +0800

    Only sync if enable_sync

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,7 +37,7 @@ class OrdersController < SecureController
     order = Order.build order_params, order_partner
 
     if order.save and order.complete!
-      SyncOrderJob.perform_later order.partner.name, order_params
+      SyncOrderJob.perform_later order.partner.name, order_params if Rails.configuration.x.cocca_api_sync
 
       render  json: order,
               status: :created,
