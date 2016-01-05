e885fa52e338c675ad7e09e9035afead0e6f36b9
Ealden Esto E. Escañan <ealden@dot.ph>
2016-01-05 13:41:11 +0800

    Flag to enable_sync

--- config/initializers/cocca-api.rb
+++ config/initializers/cocca-api.rb
@@ -1,3 +1,4 @@
 config = Rails.application.config_for('cocca-api').with_indifferent_access
 
 Rails.configuration.x.cocca_api_host = config[:host]
+Rails.configuration.x.cocca_api_sync = config[:enable_sync]
