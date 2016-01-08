e3a30cecac69cb21caaaae71bf9a93deac49d6a9
Ealden Esto E. Escañan <ealden@dot.ph>
2016-01-08 16:59:00 +0800

    Removed configured Resque workers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,4 @@
-set :branch, 'production'
-
 role :app, %w{deploy@registry.production.local}
 role :db,  %w{deploy@registry-db.production.local}
 
-# role :resque_worker, %w{deploy@registry.production.local}
-# role :resque_scheduler, %w{deploy@registry.production.local}
+set :branch, 'production'
