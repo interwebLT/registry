b18ce2966af7fccfcbd26a40648c19d3a66ac6dd
Ealden Esto E. Escañan <ealden@dot.ph>
2016-01-08 17:33:15 +0800

    Use Sidekiq as ActiveJob backend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Use Sidekiq as ActiveJob backend
+  config.active_job.queue_adapter = :sidekiq
 end
