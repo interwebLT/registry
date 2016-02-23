92d9cc776734e356680b735cbc25519839ac3027
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-23 20:17:34 +0800

    Use DiscardAdapter for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,4 +25,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Discard jobs
+  config.active_job.queue_adapter = :discard
 end
