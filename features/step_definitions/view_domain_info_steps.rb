2d397ab191bd95cff607e7431a192438a0f5c940
Ealden Esto E. Escañan <ealden@dot.ph>
2016-03-29 12:43:40 +0800

    Scenario: View domain owned by another partner

--- features/step_definitions/view_domain_info_steps.rb
+++ features/step_definitions/view_domain_info_steps.rb
@@ -4,8 +4,19 @@ When /^I try to view the info of a domain that I own$/ do
   get domain_url(domain.id)
 end
 
+When /^I try to view the info a domain owned by another partner$/ do
+  domain  = FactoryGirl.create :domain
+  domain.update partner: FactoryGirl.create(:other_partner)
+
+  get domain_url(domain.id)
+end
+
 Then /^I must see the info of my domain$/ do
   expect(last_response.status).to eq 200
 
   expect(json_response).to eql 'domains/domain.ph/get_response'.json
+end
+
+Then /^I must not be able to see the domain$/ do
+  expect(last_response.status).to eq 404
 end
--- features/view_domain_info.feature
+++ features/view_domain_info.feature
@@ -1,6 +1,12 @@
 Feature: View Domain Info
 
-  Scenario: View domain
+  Background:
     Given I am authenticated as partner
+
+  Scenario: View domain
     When  I try to view the info of a domain that I own
     Then  I must see the info of my domain
+
+  Scenario: View domain owned by another partner
+    When  I try to view the info a domain owned by another partner
+    Then  I must not be able to see the domain
