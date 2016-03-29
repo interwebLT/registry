69486a1749d0568f483d301ef715027ddcab530b
Ealden Esto E. Escañan <ealden@dot.ph>
2016-03-29 13:55:25 +0800

    Grouped routes together

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
-  resources :welcome, only: [:index]
-
-  resources :authorizations, only: [:create, :show]
+  resources :welcome,         only: [:index]
+  resources :authorizations,  only: [:create, :show]
+  resources :orders,          only: [:index, :create, :show]
+  resources :credits,         only: [:index, :create, :show]
+  resources :activities,      only: [:index]
+  resources :migrations,      only: [:create]
+  resources :contacts,        only: [:index, :create, :show, :update]
 
   resources :user, only: [:index] do
     collection do
@@ -19,23 +23,13 @@ Rails.application.routes.draw do
 
   get 'availability', to: 'availabilities#index'
 
-  resources :partners, only: [:index, :show] do 
-    resources :users, only: [:create] 
+  resources :partners, only: [:index, :show] do
+    resources :users, only: [:create]
   end
-
-  resources :orders, only: [:index, :create, :show]
-
-  resources :credits, only: [:index, :create, :show]
-
-  resources :contacts, only: [:index, :create, :show, :update]
 
   resources :hosts, only: [:index, :create, :show], id: /.*/ do
     resources :addresses, controller: :host_addresses, only: [:create, :show, :destroy]
   end
-
-  resources :activities, only: [:index]
-
-  resources :migrations, only: [:create]
 
   match '*unmatched', to: 'application#handle_routing_error', via: :all
 end
