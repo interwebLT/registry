ebc66867c0fba917f35b37f76e4421657fdaf9e1
Ealden Esto E. Escañan <ealden@dot.ph>
2016-04-05 13:08:59 +0800

    Start converting Delete Domain Admin feature from services

--- features/step_definitions/delete_domain_steps.rb
+++ features/step_definitions/delete_domain_steps.rb
@@ -1,18 +1,18 @@
 When  /^I delete a domain that currently exists$/ do
-  domain_exists
-  domain_host_exists
+  domain = FactoryGirl.create :domain
+  FactoryGirl.create :domain_host, product: domain.product
 
-  delete_domain
+  domain.destroy
 end
 
-Then  /^domain must no longer exist$/ do
-  assert_domain_does_not_exist
+Then /^domain must no longer exist$/ do
+  expect(Domain.last).to be nil
 end
 
-Then  /^domain must now be in the deleted domain list$/ do
-  assert_deleted_domain_exists
+Then /^domain must now be in the deleted domain list$/ do
+  expect(DeletedDomain.last).to have_attributes name: 'domain.ph'
 end
 
-Then  /^deleted domain must not have domain hosts$/ do
-  assert_deleted_domain_must_not_have_domain_hosts
+Then /^deleted domain must not have domain hosts$/ do
+  expect(DeletedDomain.last.product.domain_hosts).to be_empty
 end
