00cdee26eca3faceb01f70a40aa587122a271b9d
Ealden Esto E. Escañan <ealden@dot.ph>
2016-05-16 17:57:19 +0800

    Initial Application model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516061148) do
+ActiveRecord::Schema.define(version: 20160516095017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.integer "partner_id",            null: false
+    t.string  "token",      limit: 32, null: false
+  end
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "token",              limit: 32, null: false
