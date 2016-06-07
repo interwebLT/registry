fc7fddbca69c43abcbe6a4e076727bd44ff7f881
Ealden Esto E. Escañan <ealden@dot.ph>
2016-06-07 10:48:58 +0800

    Move from OrderDetail::MigrateDomain to MigratedDomain

---
+++ db/migrate/20160607024553_move_from_order_detail_migrate_domain_to_migrated_domain.rb
@@ -0,0 +1,15 @@
+class MoveFromOrderDetailMigrateDomainToMigratedDomain < ActiveRecord::Migration
+  def change
+    OrderDetail::MigrateDomain.all.each do |order_detail|
+      MigrateDomain.create  name:               order_detail.domain,
+                            registrant_handle:  order_detail.registrant_handle,
+                            registered_at:      order_detail.registered_at,
+                            expires_at:         order_detail.expires_at,
+                            authcode:           order_detail.authcode,
+                            created_at:         order_detail.order.ordered_at,
+                            updated_at:         order_detail.order.created_at
+    end
+
+    OrderDetail::MigrateDomain.destroy_all
+  end
+end
