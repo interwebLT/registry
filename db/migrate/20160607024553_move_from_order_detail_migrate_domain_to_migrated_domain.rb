bc71456ced20c45e6ab0fceda036edf9bcf10841
Ealden Esto E. Escañan <ealden@dot.ph>
2016-06-07 11:09:59 +0800

    Raise an error if MigratedDomain cannot be saved

--- db/migrate/20160607024553_move_from_order_detail_migrate_domain_to_migrated_domain.rb
+++ db/migrate/20160607024553_move_from_order_detail_migrate_domain_to_migrated_domain.rb
@@ -1,13 +1,13 @@
 class MoveFromOrderDetailMigrateDomainToMigratedDomain < ActiveRecord::Migration
   def change
     OrderDetail::MigrateDomain.all.each do |order_detail|
-      MigratedDomain.create name:               order_detail.domain,
-                            registrant_handle:  order_detail.registrant_handle,
-                            registered_at:      order_detail.registered_at,
-                            expires_at:         order_detail.expires_at,
-                            authcode:           order_detail.authcode,
-                            created_at:         order_detail.order.ordered_at,
-                            updated_at:         order_detail.order.created_at
+      MigratedDomain.create!  name:               order_detail.domain,
+                              registrant_handle:  order_detail.registrant_handle,
+                              registered_at:      order_detail.registered_at,
+                              expires_at:         order_detail.expires_at,
+                              authcode:           order_detail.authcode,
+                              created_at:         order_detail.order.ordered_at,
+                              updated_at:         order_detail.order.created_at
 
       order_detail.order.destroy
     end
