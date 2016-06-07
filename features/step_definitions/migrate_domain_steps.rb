4993b5ee3407059b638df7d8e082f04ff3aba0c9
Ealden Esto E. Escañan <ealden@dot.ph>
2016-06-07 09:20:38 +0800

    Expand When steps

--- features/step_definitions/migrate_domain_steps.rb
+++ features/step_definitions/migrate_domain_steps.rb
@@ -1,11 +1,37 @@
-MIGRATE_DOMAIN = Transform /^migrate a domain into registry(?: |)(.*?)$/ do |scenario|
-  build_request scenario: scenario, resource: :order, action: :post_migrate_domain
-end
-
-When /^I (#{MIGRATE_DOMAIN})$/ do |request|
+When /^I migrate a domain into registry for another partner$/ do
   create :contact
 
-  post migrations_path, request.json
+  post migrations_path, 'orders/admin_post_migrate_domain_request'.json
+end
+
+When /^I migrate a domain into registry for another partner with no domain name$/ do
+  create :contact
+
+  post migrations_path, 'orders/admin_post_migrate_domain_with_no_domain_name_request'.json
+end
+
+When /^I migrate a domain into registry for another partner with no registrant handle$/ do
+  create :contact
+
+  post migrations_path, 'orders/admin_post_migrate_domain_with_no_registrant_handle_request'.json
+end
+
+When /^I migrate a domain into registry for another partner with no registered at$/ do
+  create :contact
+
+  post migrations_path, 'orders/admin_post_migrate_domain_with_no_registered_at_request'.json
+end
+
+When /^I migrate a domain into registry for another partner with no expires at$/ do
+  create :contact
+
+  post migrations_path, 'orders/admin_post_migrate_domain_with_no_expires_at_request'.json
+end
+
+When /^I migrate a domain into registry for another partner with expires at before registered at$/ do
+  create :contact
+
+  post migrations_path, 'orders/admin_post_migrate_domain_with_expires_at_before_registered_at_request'.json
 end
 
 Then /^domain must be migrated under non-admin partner$/ do
