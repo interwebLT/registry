8cba2d238f50bd7a2387372edee4c4acbb567ddf
Ealden Esto E. Escañan <ealden@dot.ph>
2016-06-21 10:26:11 +0800

    CASCADE DELETE to HostAddress

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,7 +1,7 @@
 class Host < ActiveRecord::Base
   belongs_to :partner
 
-  has_many :host_addresses
+  has_many :host_addresses, dependent: :destroy
 
   validates :partner, presence: true
   validates :name,    presence: true
