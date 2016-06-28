29401213b2720bdf120cc4da23cf47c868a2a818
Ealden Esto E. Escañan <ealden@dot.ph>
2016-06-28 14:55:37 +0800

    Scenario: Remove domain host before domain is registered

--- features/remove_domain_host.feature
+++ features/remove_domain_host.feature
@@ -14,3 +14,8 @@ Feature: Remove Domain Host
     When  I try to remove a domain host from an existing domain
     Then  domain host must be removed
     And   remove domain host must not be synced to external registries
+
+  Scenario: Remove domain host before domain is registered
+    When  I try to remove a domain host before domain is registered
+    Then  domain must be checked until registered
+    Then  domain host must be removed
--- features/step_definitions/remove_domain_host_steps.rb
+++ features/step_definitions/remove_domain_host_steps.rb
@@ -2,6 +2,23 @@ When /^I try to remove a domain host from an existing domain$/ do
   domain      = FactoryGirl.create :domain
   domain_host = FactoryGirl.create :domain_host, product: domain.product
 
+  stub_request(:get, 'http://localhost:9001/domains/domain.ph')
+    .to_return status: 200, body: 'domains/domain.ph/get_response'.body
+
+  stub_request(:delete, 'http://localhost:9001/domains/domain.ph/hosts/ns5.domains.ph')
+    .to_return status: 200, body: 'domains/domain.ph/hosts/ns5.domains.ph/get_response'.body
+
+  delete domain_host_path(domain.name, domain_host.name)
+end
+
+When /^I try to remove a domain host before domain is registered$/ do
+  domain      = FactoryGirl.create :domain
+  domain_host = FactoryGirl.create :domain_host, product: domain.product
+
+  stub_request(:get, 'http://localhost:9001/domains/domain.ph')
+    .to_return(status: 404, body: 'common/404'.body).times(9)
+    .to_return status: 200, body: 'domains/domain.ph/get_response'.body
+
   stub_request(:delete, 'http://localhost:9001/domains/domain.ph/hosts/ns5.domains.ph')
     .to_return status: 200, body: 'domains/domain.ph/hosts/ns5.domains.ph/get_response'.body
 
