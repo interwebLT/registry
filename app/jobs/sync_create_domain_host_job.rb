d82ad8243d8adea25afd83be4fafc1fe8122f1c9
Ealden Esto E. Escañan <ealden@dot.ph>
2016-06-28 14:45:24 +0800

    Scenarios: Add domain host when domain is not registered (yet)

--- app/jobs/sync_create_domain_host_job.rb
+++ app/jobs/sync_create_domain_host_job.rb
@@ -1,10 +1,19 @@
 class SyncCreateDomainHostJob < ApplicationJob
   queue_as :sync_registry_changes
 
-  def perform url, domain_host
+  def perform url, domain_host, retry_count = 0
     domain = domain_host.product.domain
 
-    domain_host_url = "#{url}/domains/#{domain.name}/hosts"
+    domain_url      = "#{url}/domains/#{domain.name}"
+    domain_host_url = "#{domain_url}/hosts"
+
+    raise 'Max retry reached!' unless retry_count < MAX_SYNC_RETRY_COUNT
+
+    unless check domain_url, token: domain.partner.name
+      SyncCreateDomainHostJob.perform_later url, domain_host, (retry_count + 1)
+
+      return
+    end
 
     body = {
       name: domain_host.name
--- features/step_definitions/add_domain_host_steps.rb
+++ features/step_definitions/add_domain_host_steps.rb
@@ -1,10 +1,33 @@
 When /^I try to add a domain host to an existing domain$/ do
   domain = FactoryGirl.create :domain
 
+  stub_request(:get, 'http://localhost:9001/domains/domain.ph')
+    .to_return status: 200, body: 'domains/domain.ph/get_response'.body
+
   stub_request(:post, 'http://localhost:9001/domains/domain.ph/hosts')
     .to_return status: 200, body: 'domains/domain.ph/hosts/post_response'.body
 
   post domain_hosts_path(domain.name), 'domains/domain.ph/hosts/post_request'.json
+end
+
+When /^I try to add a domain host before domain is registered$/ do
+  domain = FactoryGirl.create :domain
+
+  stub_request(:get, 'http://localhost:9001/domains/domain.ph')
+    .to_return(status: 404, body: 'common/404'.body).times(9)
+    .to_return status: 200, body: 'domains/domain.ph/get_response'.body
+
+  stub_request(:post, 'http://localhost:9001/domains/domain.ph/hosts')
+    .to_return status: 200, body: 'domains/domain.ph/hosts/post_response'.body
+
+  post domain_hosts_path(domain.name), 'domains/domain.ph/hosts/post_request'.json
+end
+
+When /^I try to add a domain host where domain does not exist$/ do
+  domain = FactoryGirl.create :domain
+
+  stub_request(:get, 'http://localhost:9001/domains/domain.ph')
+    .to_return(status: 404, body: 'common/404'.body)
 end
 
 Then /^domain must now have domain host$/ do
@@ -17,4 +40,14 @@ end
 Then /^add domain host must be synced to external registries$/ do
   expect(WebMock).to have_requested(:post, 'http://localhost:9001/domains/domain.ph/hosts')
     .with headers: default_headers, body: 'domains/domain.ph/hosts/post_request'.json
+end
+
+Then /^add domain host must reach max retries$/ do
+  begin
+    post domain_hosts_path(Domain.last.name), 'domains/domain.ph/hosts/post_request'.json
+
+    fail
+  rescue Exception => e
+    expect(e).to be_an_instance_of RuntimeError
+  end
 end
