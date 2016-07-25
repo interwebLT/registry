e4c14a7f128f91f19036bc67fec4586ee100b154
kian1213 <chrono0424@gmail.com>
2016-07-25 08:53:32 +0800

    add start_date and active column to powerdns record serializer

--- app/serializers/powerdns/record_serializer.rb
+++ app/serializers/powerdns/record_serializer.rb
@@ -1,4 +1,4 @@
 class Powerdns::RecordSerializer < ActiveModel::Serializer
-  attributes  :id, :powerdns_domain_id, :name, :type, :content, :end_date,
-              :ttl, :prio, :change_date, :created_at, :updated_at
+  attributes  :id, :powerdns_domain_id, :name, :type, :content, :start_date, :end_date,
+              :active, :ttl, :prio, :change_date, :created_at, :updated_at
 end
