8cfe0c6241db25872af403d5ce066281b8a04ec8
kian1213 <chrono0424@gmail.com>
2016-08-09 13:22:12 +0800

    set queue adapter in development to sidekiq(will revert if needed)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,12 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Do not execute jobs
-  config.active_job.queue_adapter = :test
+  # config.active_job.queue_adapter = :test
+  config.active_job.queue_adapter = :sidekiq
 
   #For Mailcathcer gem
-  config.action_mailer.default_url_options = {host: "localhost:3000"}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {address: "localhost",
-    port: "1025"}
+  # config.action_mailer.default_url_options = {host: "localhost:3000"}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {address: "localhost",
+  #   port: "1025"}
 end
