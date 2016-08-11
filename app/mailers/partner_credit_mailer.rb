c0ba8985519f512ce82dbaaff3daf1e35eac83a8
kian1213 <chrono0424@gmail.com>
2016-08-11 14:02:02 +0800

    add credit notice mail content

--- app/mailers/partner_credit_mailer.rb
+++ app/mailers/partner_credit_mailer.rb
@@ -1,12 +1,17 @@
 class PartnerCreditMailer < ApplicationMailer
+  def credit_balance_below_100_notification order
+    @partner = order.partner
+    mail to: order.partner.email, subject: "Low Balance Notice"
+  end
+
   def credit_balance_notification order
     @partner = order.partner
-    mail to: order.partner.email, subject: "Partner Credit Balance"
+    mail to: order.partner.email, subject: "Credit Balance Notice"
   end
 
   def credit_limit_notice order
     @partner = order.partner
     @order = order
-    mail to: order.partner.email, subject: "Partner Credit Limit Reminder"
+    mail to: order.partner.email, subject: "Credit Limit Notice"
   end
 end
