977879fab90e468146dc06dddbe5cae8fb8ee509
kian1213 <chrono0424@gmail.com>
2016-08-12 11:11:51 +0800

    add delay to credit notice mailer

--- app/mailers/partner_credit_mailer.rb
+++ app/mailers/partner_credit_mailer.rb
@@ -1,17 +1,29 @@
 class PartnerCreditMailer < ApplicationMailer
-  def credit_balance_below_100_notification order
+  def credit_balance_below_threshold order
     @partner = order.partner
-    mail to: order.partner.email, subject: "Low Balance Notice"
+    mail to: @partner.email, subject: "Low Balance Notice"
   end
 
   def credit_balance_notification order
     @partner = order.partner
-    mail to: order.partner.email, subject: "Credit Balance Notice"
+    mail to: @partner.email, subject: "Credit Balance Notice"
   end
 
-  def credit_limit_notice order
+  def credit_limit_notice order, notice_level
     @partner = order.partner
     @order = order
-    mail to: order.partner.email, subject: "Credit Limit Notice"
+
+    case notice_level
+      when "first"
+        if @partner.preferences["cl_second_notice"].nil?
+          mail to: @partner.email, subject: "Credit Limit Notice"
+        end
+      when "second"
+        if @partner.preferences["cl_third_notice"].nil?
+          mail to: @partner.email, subject: "Credit Limit Notice"
+        end
+      when "third"
+        mail to: @partner.email, subject: "Credit Limit Notice"
+    end
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -16,7 +16,6 @@ class Order < ActiveRecord::Base
 
   before_create :generate_order_number
   before_create :get_credit_before_create
-  before_create :check_credit_limit_if_notified
 
   after_save :check_credit_balance
   after_save :check_credit_limit_percentage
@@ -133,12 +132,12 @@ class Order < ActiveRecord::Base
       current_balance   = ActionController::Base.helpers.humanized_money(self.partner.current_balance).gsub(',','').to_i
       if balance_before_transaction > Rails.configuration.cl_notice_threshold.to_i
         if current_balance < Rails.configuration.cl_notice_threshold.to_i
-          PartnerCreditMailer.credit_balance_below_100_notification(self).deliver_now
+          PartnerCreditMailer.delay_for(5.minute, queue: "registry_mailer").credit_balance_below_threshold(self)
         end
       end
       if balance_before_transaction > 0
         if current_balance < 0
-          PartnerCreditMailer.credit_balance_notification(self).deliver_now
+          PartnerCreditMailer.delay_for(5.minute, queue: "registry_mailer").credit_balance_notification(self)
         end
       end
     end
@@ -155,20 +154,30 @@ class Order < ActiveRecord::Base
         credit_limit_third_boundary  = @credit_limit * Rails.configuration.third_notice.to_f
 
         if credit_limit_first_boundary > current_credit_limit
-          unless @first_notice
-            PartnerCreditMailer.credit_limit_notice(self).deliver_now
+          if self.partner.preferences.nil?
+            self.partner.preferences = {cl_first_notice: "true" }
+            self.partner.save!
+            PartnerCreditMailer.delay_for(5.minute, queue: "registry_mailer").credit_limit_notice(self, "first")
           end
         end
 
         if credit_limit_second_boundary > current_credit_limit
-          unless @second_notice
-            PartnerCreditMailer.credit_limit_notice(self).deliver_now
+          unless self.partner.preferences.nil?
+            if self.partner.preferences["cl_second_notice"].nil?
+              self.partner.preferences["cl_second_notice"] = true
+              self.partner.save!
+              PartnerCreditMailer.delay_for(5.minute, queue: "registry_mailer").credit_limit_notice(self, "second")
+            end
           end
         end
 
         if credit_limit_third_boundary > current_credit_limit
-          unless @third_notice
-            PartnerCreditMailer.credit_limit_notice(self).deliver_now
+          unless self.partner.preferences.nil?
+            if self.partner.preferences["cl_third_notice"].nil?
+              self.partner.preferences["cl_third_notice"] = true
+              self.partner.save!
+              PartnerCreditMailer.delay_for(5.minute, queue: "registry_mailer").credit_limit_notice(self, "third")
+            end
           end
         end
       end
@@ -179,37 +188,6 @@ class Order < ActiveRecord::Base
     unless partner.nil?
       @current_balance = ActionController::Base.helpers.humanized_money(self.partner.current_balance).gsub(/,/,'').to_i
       @credit_limit    = self.partner.credit_limit.to_i
-    end
-  end
-
-  def check_credit_limit_if_notified
-    unless partner.nil?
-      if self.status == COMPLETE_ORDER && Rails.configuration.cl_notice_active
-        current_balance   = ActionController::Base.helpers.humanized_money(self.partner.current_balance).gsub(',','').to_i
-
-        if current_balance < 0
-          current_credit_limit = current_balance + @credit_limit
-          @first_notice        = false
-          @second_notice       = false
-          @third_notice        = false
-
-          credit_limit_first_boundary  = @credit_limit * Rails.configuration.first_notice.to_f
-          credit_limit_second_boundary = @credit_limit * Rails.configuration.second_notice.to_f
-          credit_limit_third_boundary  = @credit_limit * Rails.configuration.third_notice.to_f
-
-          if credit_limit_first_boundary > current_credit_limit
-            @first_notice = true
-          end
-
-          if credit_limit_second_boundary > current_credit_limit
-            @second_notice = true
-          end
-
-          if credit_limit_third_boundary > current_credit_limit
-            @third_notice = true
-          end
-        end
-      end
     end
   end
 
