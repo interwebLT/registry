424e2e1d3703c5d0435920951ad9aa1810564db7
kian1213 <chrono0424@gmail.com>
2016-08-16 15:55:42 +0800

    revert

--- app/models/host.rb
+++ app/models/host.rb
@@ -6,17 +6,4 @@ class Host < ActiveRecord::Base
   validates :partner, presence: true
   validates :name,    presence: true
   validates :name,    uniqueness: true
-
-  after_save :sync_create
-
-  private
-
-  def sync_create
-    ExternalRegistry.all.each do |registry|
-      next if registry.name == self.partner.client
-      next if ExcludedPartner.exists? name: self.partner.name
-
-      SyncCreateHostJob.perform_later registry.url, self
-    end
-  end
 end
