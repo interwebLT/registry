20ad4ba4f7b461ff1f265917cb9f8c311e16b389
kian1213 <chrono0424@gmail.com>
2016-08-16 14:59:03 +0800

    try add sync methods to model, (will revert if not needed)

--- app/models/host.rb
+++ app/models/host.rb
@@ -6,4 +6,17 @@ class Host < ActiveRecord::Base
   validates :partner, presence: true
   validates :name,    presence: true
   validates :name,    uniqueness: true
+
+  after_save :sync_create
+
+  private
+
+  def sync_create
+    ExternalRegistry.all.each do |registry|
+      next if registry.name == self.partner.client
+      next if ExcludedPartner.exists? name: self.partner.name
+
+      SyncCreateHostJob.perform_later registry.url, self
+    end
+  end
 end
--- app/models/host_address.rb
+++ app/models/host_address.rb
@@ -7,4 +7,17 @@ class HostAddress < ActiveRecord::Base
   validates :address, presence: true
   validates :address, uniqueness: { scope: [:address, :host] }
   validates :type, inclusion: { in: %w(v4 v6) }
+
+  after_save :sync_create
+
+  private
+
+  def sync_create
+    ExternalRegistry.all.each do |registry|
+      next if registry.name == self.host.partner.client
+      next if ExcludedPartner.exists? name: self.host.partner.name
+
+      SyncCreateHostAddressJob.perform_later registry.url, self
+    end
+  end
 end
