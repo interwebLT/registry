750acb088859fb53f823f0f8b55d05abba62a722
kian1213 <chrono0424@gmail.com>
2016-08-26 18:14:31 +0800

    update domain host ip_list in host_address callback

--- app/models/host_address.rb
+++ app/models/host_address.rb
@@ -7,4 +7,46 @@ class HostAddress < ActiveRecord::Base
   validates :address, presence: true
   validates :address, uniqueness: { scope: [:address, :host] }
   validates :type, inclusion: { in: %w(v4 v6) }
+
+  before_destroy :get_host_name
+  after_destroy :update_domain_host_ip_list_if_any
+  after_save :update_domain_host_ip_list_if_any
+
+  private
+
+  def get_host_name
+    @host = self.host
+  end
+
+  def update_domain_host_ip_list_if_any
+    if @host.nil?
+      @host = self.host
+    end
+
+    domain_host    = DomainHost.find_by_name(@host.name)
+    host_addresses = @host.host_addresses
+
+    unless domain_host.nil?
+      unless host_addresses.empty?
+        ipv4 = []
+        ipv6 = []
+        host_addresses.map{ |host_address|
+          if host_address.address.length > 15
+            ipv6 << host_address.address
+          else
+            ipv4 << host_address.address
+          end
+        }
+
+        v4_hash = Hash[(0...ipv4.size).zip ipv4]
+        v6_hash = Hash[(0...ipv6.size).zip ipv6]
+
+        list = {"ipv4": v4_hash, "ipv6": v6_hash}.to_json
+
+        domain_host.ip_list = list
+        domain_host.update_ip_list_from_host = true
+        domain_host.save(validate: false)
+      end
+    end
+  end
 end
