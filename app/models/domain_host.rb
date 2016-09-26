1f5262fdbc406ed94d5d7e3bf713e20022f96d59
kian1213 <chrono0424@gmail.com>
2016-09-26 15:46:30 +0800

    SOA record prio defaut value should be empty

--- app/models/domain_host.rb
+++ app/models/domain_host.rb
@@ -72,7 +72,6 @@ class DomainHost < ActiveRecord::Base
       Powerdns::Record.find_or_create_by(powerdns_domain_id: powerdns_domain.id) do |powerdns_record|
         powerdns_record.name = powerdns_domain.name
         powerdns_record.type = "SOA"
-        powerdns_record.prio = 0
         powerdns_record.content = "ns5.domains.ph root.ns5.domains.ph #{date_today}01 28800 7200 864000 14400"
         powerdns_record.end_date = domain.expires_at
       end
