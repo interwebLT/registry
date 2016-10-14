9965fdbe253a86646384d6124f96209c8b709f0f
kian1213 <chrono0424@gmail.com>
2016-10-14 16:23:14 +0800

    host address array check on delete host address job

--- app/jobs/sync_delete_bulk_host_address_job.rb
+++ app/jobs/sync_delete_bulk_host_address_job.rb
@@ -2,9 +2,31 @@ class SyncDeleteBulkHostAddressJob < ApplicationJob
   queue_as :sync_registry_changes
 
   def perform url, host, ip_array
-    host_url          = "#{url}/hosts/#{host.name}"
-    host_address_url  = "#{host_url}/addresses/#{ip_array.first}?ip_list=#{ip_array.join(",")}"
+    new_ip_list = []
+    header   = {"Content-Type"=>"application/json", "Accept"=>"application/json", "Authorization"=>"Token token=#{host.partner.name}"}
+    headers  = {headers: header}
 
-    delete host_address_url, token: host.partner.name
+    ip_array.each do |host_address|
+      host_url = "#{url}/hosts/#{host.name}/#{host_address}"
+
+      host_address_available_in_cocca = process_response HTTParty.get(host_url, headers)
+
+      if host_address_available_in_cocca
+        new_ip_list << host_address
+      end
+    end
+
+    unless new_ip_list.empty?
+      host_address_url  = "#{host_url}/addresses/#{new_ip_list.first}?ip_list=#{new_ip_list.join(",")}"
+      delete host_address_url, token: host.partner.name
+    end
+  end
+
+  def process_response response
+    if (400..599).include? response.code
+      false
+    else
+      true
+    end
   end
 end
