d02c59ff4d3c0b6d63b7c19d5ff4f10aa666e5b1
kian1213 <chrono0424@gmail.com>
2016-10-18 16:01:37 +0800

    update models for expiration update rake task

--- app/models/cocca/mismatch_sinag_domain_exdate.rb
+++
@@ -1,3 +0,0 @@
-class Cocca::MismatchSinagDomainExdate < ActiveRecord::Base
-  self.table_name = 'check_sinag_cocca_expirydate_troypartner'
-end
---
+++ app/models/cocca/mismatch_sinag_domain_exdate_epp_partner.rb
@@ -0,0 +1,3 @@
+class Cocca::MismatchSinagDomainExdateEppPartner < ActiveRecord::Base
+  self.table_name = 'check_sinag_cocca_expirydate_epppartner'
+end
