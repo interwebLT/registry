c95f9488cfd8edcc3c6c42115e436d5e396cdf03
kian1213 <chrono0424@gmail.com>
2016-10-20 16:48:56 +0800

    models for host address resync

---
+++ app/models/cocca/mismatch_host_address_to_cocca.rb
@@ -0,0 +1,3 @@
+class Cocca::MismatchHostAddressToCocca < ActiveRecord::Base
+  self.table_name = "check_sinag_cocca_hostaddress_troypartner"
+end
---
+++ app/models/cocca/mismatch_host_address_to_sinag.rb
@@ -0,0 +1,3 @@
+class Cocca::MismatchHostAddressToSinag < ActiveRecord::Base
+  self.table_name = "check_sinag_cocca_hostaddress_epppartner"
+end
