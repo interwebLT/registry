1cb75a4e057500535051800ea7ebc4b1cde67928
kian1213 <chrono0424@gmail.com>
2016-10-28 09:13:50 +0800

    pricing serializer sort

--- app/models/partner_pricing.rb
+++ app/models/partner_pricing.rb
@@ -4,4 +4,20 @@ class PartnerPricing < ActiveRecord::Base
   monetize :price_cents
 
   belongs_to :partner
+
+  def self.sorted_by_transfer_register_renew
+    PartnerPricing.all.sort_by(&:sort_action)
+  end
+
+  def sort_action
+    if self.action == "transfer_domain"
+      position = 1
+    elsif self.action == "domain_create"
+      position = 2
+    elsif self.action == "domain_renew"
+      position = 3
+    else
+    end
+    return position
+  end
 end
