ded4475f0fb6e38165f588027f1e46f7409d32d0
kian1213 <chrono0424@gmail.com>
2016-11-04 10:56:24 +0800

    troy users scope for list of partners

--- app/models/troy/user.rb
+++ app/models/troy/user.rb
@@ -1,4 +1,6 @@
 class Troy::User < ActiveRecord::Base
   self.table_name = 'troy_users'
   self.inheritance_column = :_type_disabled
+
+  scope :partners, -> {where("trim(type) = ? and userid not like ? and userid not like ?", "partner", "cp%", "tp%")}
 end
