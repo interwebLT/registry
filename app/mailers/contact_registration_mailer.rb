6da1c785086f82712e4fce5e834edd66ee2acd1d
kian1213 <chrono0424@gmail.com>
2016-11-16 14:39:58 +0800

    contact non ascii mailer, include handle

--- app/mailers/contact_registration_mailer.rb
+++ app/mailers/contact_registration_mailer.rb
@@ -1,7 +1,8 @@
 class ContactRegistrationMailer < ApplicationMailer
-  def non_ascii_notification partner, contact
+  def non_ascii_notification partner, handle, contact
     @partner = partner
     @contact = contact
+    @handle  = handle
     mail to: @partner.email, bcc: ["registrar@dot.ph"], subject: "Contact Registration Notice"
   end
 end
